e50ff6af81a69cf043b6a150eda3269221907e02
Al-Aiz Hashim <AlAiz.Hashim@gmail.com>
2020-11-22 12:38:53 +0300

    news_api

--- api/news_api.php
+++ api/news_api.php
@@ -2,28 +2,99 @@
 include('../model/news.php');
 $news_model=new News();
 
-if(isset($_POST)&&!empty($_POST)){
-    $news_model->news_title=$_POST['title'];
-    $news_model->news_region=$_POST['region'];
-    $news_model->news_details=$_POST['details'];
 
-if($news_model->addRow()){
-    $feedback['code']=200;
-    $feedback['message']="data inserted successfully";
 
-}else{
-    $feedback['code']=400;
-    $feedback['message']="failed to inserted data";
+
+ if(isset($_POST)&&!empty($_POST)){
+    $news_model->news_category = $_POST['category'];
+    $news_model->news_region = $_POST['region'];
+    $news_model->news_title = $_POST['title'];
+    $news_model->news_details = $_POST['details'];
+
+    if ($news_model->addRow()){
+        $feedback['code'] = 200;
+        $feedback['message'] = "data inserted successfully";
+
+    }else{
+        $feedback['code'] = 400;
+        $feedback['message'] = "failed to insert data";
+
+    }
+    echo json_encode ($feedback);
+}
+
+
+
+
+else if($_SERVER['REQUEST_METHOD']=="PUT"){
+    $_PUT= array();
+    parse_str(file_get_contents('php://input'), $_PUT);
+    $news_model->news_category = $_PUT['category'];
+    $news_model->news_region = $_PUT['region'];
+    $news_model->news_title = $_PUT['title'];
+    $news_model->news_details = $_PUT['details'];
+    $news_model->news_id = $_PUT['id'];
+
+
+
+    if ($news_model->updateRow()){
+        $feedback['code'] = 200;
+        $feedback['message'] = "row ".$_PUT['id']." updated successfully";
+
+    }else{
+        $feedback['code'] = 400;
+        $feedback['message'] = "failed to update row ".$_PUT['id'];
+
+    }
+    echo json_encode ($feedback);
+
 
 }
- echo json_encode($feedback);
+else if($_SERVER['REQUEST_METHOD']=="DELETE"){
+    // $_DELETE=array();
+    //parse_str(file_get_contents('php://input'), $_DELETE);
+
+    $news_model->news_id = $_GET['id'];
+    if ($news_model->deleteRow()){
+        $feedback['code'] = 200;
+        $feedback['message'] = "the row  is deleted successfully";
+
+    }else{
+        $feedback['code'] = 400;
+        $feedback['message'] = "failed to delete row ";
+
+    }
+    echo json_encode ($feedback);
+
+
 }
+else if(isset($_GET)){
+    if (isset($_GET['id'])){
+        echo json_encode ($news_model->getSingleRow($_GET['id']));
+    }
+    else if (isset($_GET['category']) && isset($_GET['region'])){
+        echo json_encode ($news_model->getRowsByCategoryAndRegion($_GET['category'], $_GET['region']));
+
+    }
+    else if (isset($_GET['category']) && !isset($_GET['region'])){
+        echo json_encode ($news_model->getRowsByCategory($_GET['category']));
+
+    }
+    else if (!isset($_GET['category']) && isset($_GET['region'])){
+        echo json_encode ($news_model->getRowsByRegion($_GET['region']));
+
+    }
+    else {
+        echo json_encode ($news_model->getRows());
+    }
 
 
-//else if(isset($_GET['id'])){
-  //  echo json_encode($news_model->getSingleRow($_GET['id']));
-//}else
-//echo json_encode($news_model->getRows());
+}// end of get
+
+
+
+
+
 
 
 
--- model/news.php
+++ model/news.php
@@ -1,55 +1,103 @@
 <?PHP
 include('../db_config.php');
-class News{
-  public $news_id;
-  public $news_title ;
-  public $news_image;
-  public $news_region;
-  public $news_details;
-  public $database;
+class News {
+    public $news_id;
+    public $news_category;
+    public $news_region;
+    public $news_title;
+    public $news_image;
+    public $news_details;
+    public $database;
 
-  function __construct(){
-   $this->database=new DBConfig();
+    function __construct()
+    {
+        $this->database = new DBConfig();
 
-  }
-  
-  function addRow(){
+    }
 
-    try{
-    $pdo=$this->database->connect();
-    $statement=$pdo->prepare('insert into pnews values(null,?,?,null,now(),?)');
-    $statement->execute([$this->news_title,$this->news_region,$this->news_details]);
-    return true;
-    }catch(PDOException $ex){
-        return false;
+    function updateRow()
+    {
+        try {
+            $pdo= $this->database->connect();
+            $sql = "update pnews set category=?,region=?, title=? , info=? WHERE id=?";
+            $statement= $pdo->prepare($sql);
+            $statement->execute([$this->news_category, $this->news_region, $this->news_title, $this->news_details, $this->news_id]);
+            return true;
+        } catch (PDOException $ex) {
+            return false;
+        }
+
+    }
+
+    function getRows()
+    {
+        $pdo= $this->database->connect();
+        $statement= $pdo->prepare("select id,title from pnews");
+        $statement->execute();
+        $rows= $statement->fetchAll(PDO::FETCH_OBJ);
+        return $rows;
+    }
+    function getSingleRow($id)
+    {
+        $pdo= $this->database->connect();
+        $statement= $pdo->prepare("select * from pnews where id=?");
+        $statement->execute([$id]);
+        $row= $statement->fetchAll(PDO::FETCH_OBJ);
+        return $row;
+    }
+    function getRowsByCategoryAndRegion($category,$region)
+    {
+        $pdo= $this->database->connect();
+        $statement= $pdo->prepare("select id,title from pnews where category=? and region=?");
+        $statement->execute([$category, $region]);
+        $rows= $statement->fetchAll(PDO::FETCH_OBJ);
+        return $rows;
+    }
+    function getRowsByCategory($category)
+    {
+        $pdo= $this->database->connect();
+        $statement= $pdo->prepare("select id,title from pnews where category=?");
+        $statement->execute([$category]);
+        $rows= $statement->fetchAll(PDO::FETCH_OBJ);
+        return $rows;
+    }
+    function getRowsByRegion($region)
+    {
+        $pdo= $this->database->connect();
+        $statement= $pdo->prepare("select id,title from pnews where  region=?");
+        $statement->execute([$region]);
+        $rows= $statement->fetchAll(PDO::FETCH_OBJ);
+        return $rows;
+    }
+
+    function addRow()
+    {
+
+        try {
+            $pdo= $this->database->connect();
+            $statement= $pdo->prepare('insert into pnews values(null,?,?,?,null,now(),?)');
+            $statement->execute([  $this->news_category,$this->news_region,$this->news_title, $this->news_details]);
+            return true;
+        } catch (PDOException $ex) {
+            return false;
+        }
+
     }
 
 
-  }
+    function deleteRow()
+    {
+        try {
+            $pdo= $this->database->connect();
+            $sql = "delete from pnews WHERE id=?";
+            $statement= $pdo->prepare($sql);
+            $statement->execute([$this->news_id]);
+            return true;
+        } catch (PDOException $ex) {
+            return false;
+        }
 
-  function getRows(){
-      $pdo=$this->database->connect();
-      $statement=$pdo->prepare("select * from pnews");
-      $statement->execute();
-      $rows=$statement->fetchAll(PDO::FETCH_OBJ);
-      return $rows;
-  }
-  function getSingleRow($id){
-    $pdo=$this->database->connect();
-    $statement=$pdo->prepare("select * from pnews where p_id=?");
-    $statement->execute([$id]);
-    $row=$statement->fetchAll(PDO::FETCH_OBJ);
-    return $row;
-}
-
-
-  function updateRow($id){
-      
-}
-
-  function deleteRow($id){
-
-  }
+    }
 
 }
 
